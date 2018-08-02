3f026d8154f7efd96bc2929e447e6bf19b734b8b
wilson1745 <wilson-power0130@hotmail.com>
2018-08-02 16:24:28 +0100

    check point built successfully chart as well barchart

--- app/src/main/java/wilson/com/project_snoringtest/BarchartActivity.java
+++ app/src/main/java/wilson/com/project_snoringtest/BarchartActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -20,13 +24,14 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.ChartTouchListener;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 public class BarchartActivity extends AppCompatActivity implements OnChartGestureListener, OnChartValueSelectedListener {
 
    private static final String TAG = "LineTutorialActivity";
-   private LineChart mChart;
+   BarChart barChart;
    private ArrayList<Integer> active_list;
 
    @Override
@@ -34,28 +39,15 @@ public class BarchartActivity extends AppCompatActivity implements OnChartGestur
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_barchart);
 
-      mChart = findViewById(R.id.linechart);
-      mChart.setOnChartGestureListener(this);
-      mChart.setOnChartValueSelectedListener(this);
-      mChart.setDragEnabled(true);
-      mChart.setScaleEnabled(false);
-      mChart.animateY(3000);
+      barChart = findViewById(R.id.barchart);
 
-       /*//limit line
-      LimitLine upper_line = new LimitLine(65f, "Danger");
-      upper_line.setLineWidth(4f);
-      upper_line.enableDashedLine(10f, 10, 0f);
-      upper_line.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
-      upper_line.setTextSize(15f);
+      barChart.setDrawBarShadow(false);
+      barChart.setDrawValueAboveBar(true);
+      barChart.setMaxVisibleValueCount(50);
+      barChart.setPinchZoom(false);
+      barChart.setDrawGridBackground(true);
 
-      //limit line
-      LimitLine lower_line = new LimitLine(35f, "Too Low");
-      lower_line.setLineWidth(4f);
-      lower_line.enableDashedLine(10f, 10, 0f);
-      lower_line.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
-      lower_line.setTextSize(15f);*/
-
-      YAxis leftAxis = mChart.getAxisLeft();
+      YAxis leftAxis = barChart.getAxisLeft();
       leftAxis.removeAllLimitLines();
       //leftAxis.addLimitLine(upper_line);
       //leftAxis.addLimitLine(lower_line);
@@ -65,27 +57,50 @@ public class BarchartActivity extends AppCompatActivity implements OnChartGestur
       leftAxis.enableGridDashedLine(10f, 10f, 0);
       leftAxis.setDrawLimitLinesBehindData(true);
 
-      mChart.getAxisRight().setEnabled(false);
+      barChart.getAxisRight().setEnabled(false);
 
       //set data
-      ArrayList<Entry> yValues = new ArrayList<>();
+      ArrayList<BarEntry> yValues = new ArrayList<>();
 
       Intent intent = getIntent();
       if("send active_list".equals(intent.getAction())) {
          active_list = (ArrayList<Integer>) intent.getSerializableExtra("active_list");
          for(int i = 0;i < active_list.size(); i++){
-            Log.e("tag", "LineChartActivity:" + active_list.get(i));
-            yValues.add(new Entry(i, active_list.get(i)));
+            Log.e("tag", "BarchartActivity:" + active_list.get(i));
+            yValues.add(new BarEntry(i, active_list.get(i)));
          }
       }
 
-      //put into line data database
-      LineDataSet set1 = new LineDataSet(yValues, "Sound Value");
-      set1.setFillAlpha(110);
+      BarDataSet set1 = new BarDataSet(yValues, "Sound Value");
+      set1.setColors(ColorTemplate.COLORFUL_COLORS);
 
-      //set text, color
+      BarData data = new BarData(set1);
+
+      //space between two bars
+      float groupSpace = 0.25f;
+      float barSpace = 0.02f;
+      float barWidth = 0.3f;
+
+      barChart.setData(data);
+      //data.setBarWidth(0.9f);
+      data.setBarWidth(barWidth);
+
+      XAxis xAxis = barChart.getXAxis();
+      String[] values = new String[active_list.size()];
+      for(int i = 0; i < active_list.size(); i++) {
+         values[i] = String.valueOf(i + 1);
+      }
+      xAxis.setValueFormatter(new MyXAxisValueFormatter(values));
+      xAxis.setLabelCount(values.length, false);
+
+      xAxis.setGranularity(1);
+      xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+      //xAxis.setCenterAxisLabels(true);
+      //xAxis.setAxisMinimum(1);
+
+
+      /*//set text, color
       set1.setColor(Color.RED);
-      set1.setLineWidth(3f);
       set1.setValueTextSize(10f);
       set1.setValueTextColor(Color.BLUE);
       set1.setDrawValues(false);
@@ -105,7 +120,7 @@ public class BarchartActivity extends AppCompatActivity implements OnChartGestur
       XAxis xAxis = mChart.getXAxis();
       xAxis.setValueFormatter(new MyXAxisValueFormatter(values));
       xAxis.setGranularity(1);
-      xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+      xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);*/
    }
 
    @Override
